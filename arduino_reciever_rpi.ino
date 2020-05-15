c4648fe4cda7ba029b71204159f96fd8d40f1142
Sean04 <41533874+Sean04@users.noreply.github.com>
2020-05-15 14:56:33 +1000

    Add files via upload

---
+++ arduino_reciever_rpi.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(9, 10, 3, 4, 5, 6);
+RF24 radio(7,8);
+
+uint8_t address[] = {0xE0, 0xE0, 0xF1, 0xF1, 0xE0};
+
+//Plant data struct package sent over RF24
+struct package {
+  float temperature;
+  float light;
+  float humidity;
+  float moisture;
+};
+typedef struct package Package;
+Package data;
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.print("Ready");
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setChannel(0x76);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()){
+    radio.read(&data, sizeof(data));
+    lcd.clear();
+    lcd.print("Temp: ");
+    lcd.print(data.temperature);
+    lcd.setCursor(0,1);
+    lcd.print("Light: ");
+    lcd.print(data.light);
+    Serial.print("Temperature:"); Serial.println(data.temperature);
+    Serial.println(data.light);
+    data.moisture = map(data.moisture, 1023, 261, 0, 100);
+    Serial.println(data.moisture);
+    Serial.println(data.humidity);
+    delay(5000);
+  }
+}
